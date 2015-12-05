7ca0debc4cd84e7ce763831c95ab869cee46bb83
Peter King <pjk22@bigpond.com>
2015-12-05 17:57:10 +1100

    change I3 config z-stop normally closed

--- Sprinter/Configuration.h
+++ Sprinter/Configuration.h
@@ -107,9 +107,9 @@
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
 //If your axes are only moving in one direction, make sure the endstops are connected properly.
 //If your axes move in one direction ONLY when the endstops are triggered, set [XYZ]_ENDSTOP_INVERT to true here:
-const bool X_ENDSTOP_INVERT = true;
-const bool Y_ENDSTOP_INVERT = true;
-const bool Z_ENDSTOP_INVERT = true;
+const bool X_ENDSTOP_INVERT = true;		// normally open ie 0V equals triggered
+const bool Y_ENDSTOP_INVERT = true;		//normally open
+const bool Z_ENDSTOP_INVERT = false;	// normally closed ie 5v equals triggered
 
 // This determines the communication speed of the printer
 #define BAUDRATE 115200
